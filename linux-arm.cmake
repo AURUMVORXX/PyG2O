bc331091ab3cb020cda10dd8aeeebf8dfd3cd52f
Patrix <patrixon9999@wp.pl>
2024-03-03 14:56:14 +0100

    Removed unnecessary toolchain files

--- linux-arm.cmake
+++ linux-arm.cmake
@@ -5,22 +5,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# specify shared library suffix
-set(OUT_FILE_SUFFIX "arm")
-
-if (${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "arm.*")
-    # specify native compiler
-    set(CMAKE_C_COMPILER gcc)
-    set(CMAKE_CXX_COMPILER g++)
-else()
-    # specify cross compiler
-    set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
-    set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
-endif()
-
-# specify the compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static-libgcc -static-libstdc++")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
+# specify cross compiler
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
 
 # specify the paths for find_(name), functions (target environment). 
 set(CMAKE_FIND_ROOT_PATH
--- linux-arm64.cmake
+++ linux-arm64.cmake
@@ -5,22 +5,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# specify shared library suffix
-set(OUT_FILE_SUFFIX "arm64")
-
-if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "aarch64")
-    # specify native compiler
-    set(CMAKE_C_COMPILER gcc)
-    set(CMAKE_CXX_COMPILER g++)
-else()
-    # specify cross compiler
-    set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
-    set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-endif()
-
-# specify the compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static-libgcc -static-libstdc++")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
+# specify cross compiler
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+ set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
 # specify the paths for find_(name), functions (target environment). 
 set(CMAKE_FIND_ROOT_PATH
