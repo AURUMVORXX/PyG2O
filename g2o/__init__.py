27af4ae3c4599af8a0d4b2e912af5195b770d5fa
AURUMVORXX <aurumvorax.enthroned@gmail.com>
2024-11-06 19:45:19 +0300

    feat: Added remaining event related functionality

--- g2o/__init__.py
+++ g2o/__init__.py
@@ -3,6 +3,8 @@ from g2o.events     import addEvent
 from g2o.events     import callEvent
 from g2o.events     import event
 from g2o.events     import removeEventHandler
+from g2o.events     import toggleEvent
+from g2o.events     import removeEvent
 
 from g2o.packets    import Packet
 from g2o.damage     import DamageDescription
