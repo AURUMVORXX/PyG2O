7f3e52837450ad03eee9740e080fc116aa822c7d
AURUMVORXX <aurumvorax.enthroned@gmail.com>
2024-11-25 04:56:46 +0300

    feat: Removed CPython separate compilation
    + Upgraded to Python 3.13
    + Added constants to g2o library for auto-completion

---
+++ g2o/constants/context.py
@@ -0,0 +1,4 @@
+import sqg2oconst
+
+DAMAGE_CTX = sqg2oconst.DAMAGE_CTX
+EQUIPMENT_CTX = sqg2oconst.EQUIPMENT_CTX
---
+++ g2o/constants/general.py
@@ -0,0 +1,5 @@
+import sqg2oconst
+
+DEBUG_MODE = sqg2oconst.DEBUG_MODE
+CLIENT_SIDE = sqg2oconst.CLIENT_SIDE
+SERVER_SIDE = sqg2oconst.SERVER_SIDE
